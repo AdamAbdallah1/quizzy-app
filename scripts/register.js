037e3a462e26dd7bdbe34396cc846e0399f42ace
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-04-19 18:43:31 +0300

    Getting button click and user inputs value

---
+++ scripts/register.js
@@ -0,0 +1,10 @@
+const regBtn = document.getElementById("register-button");
+
+const username = document.getElementById("username").value;
+const email = document.getElementById("mail").value;
+const password = document.getElementById("password").value;
+
+
+regBtn.addEventListener("click", function () {
+    
+})
