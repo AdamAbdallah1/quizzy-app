fb90e82975bf7a21c4878ea8c8edb729c5247bdd
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-04-19 19:23:56 +0300

    Getting button click and user input

---
+++ scripts/login.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const logBtn = document.getElementById("login-button");
+    const errorMessage = document.getElementById("error-message");
+
+    logBtn.addEventListener("click", () => {
+        const username = document.getElementById("username").value.trim();
+        const password = document.getElementById("password").value.trim();
+        
+    })
+
+})
