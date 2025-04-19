312a22d88d7dc146d3a3528f35a75ab56a5d3654
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-04-19 19:25:28 +0300

    Setting error message if empty

--- scripts/login.js
+++ scripts/login.js
@@ -5,7 +5,14 @@ document.addEventListener("DOMContentLoaded", () => {
     logBtn.addEventListener("click", () => {
         const username = document.getElementById("username").value.trim();
         const password = document.getElementById("password").value.trim();
-        
+
+        errorMessage.textContent = "";
+        errorMessage.style.color = "#6b0000";
+
+        if (!username || !password) {
+            errorMessage.textContent = "All fields are required. Please fill them all.";
+            return;
+        }
     })
 
 })
