bac5321d32cff317dc041c48de68a14fdc0b84f4
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-04-19 23:54:24 +0300

    Fetching questions from localStorage

--- scripts/home.js
+++ scripts/home.js
@@ -1,15 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const headerUserName = document.getElementById("header-user-name");
+    const welcomeUserName = document.getElementById("welcome-user-name");
+
     const currentUserData = localStorage.getItem("currentUser");
 
     if (currentUserData) {
         const currentUser = JSON.parse(currentUserData);
-        
-        const headerName = document.getElementById("header-user-name");
-        const welcomeName = document.getElementById("welcome-user-name");
-
-        if (headerName) headerName.textContent = currentUser.logEmail;
-        if (welcomeName) welcomeName.textContent = currentUser.logUsername;
-
-        console.log(currentUser);
+        headerUserName.textContent = currentUser.regUsername || "User";
+        welcomeUserName.textContent = currentUser.regUsername || "User";
+    } else {
+        headerUserName.textContent = "Guest";
+        welcomeUserName.textContent = "Guest";
     }
+
+    const htmlQuiz = JSON.parse(localStorage.getItem("quizzyQuizHTML"));
+    const jsQuiz = JSON.parse(localStorage.getItem("quizzyQuizJS"));
+    const sqlQuiz = JSON.parse(localStorage.getItem("quizzyQuizSQL"));
+    const phpQuiz = JSON.parse(localStorage.getItem("quizzyQuizPHP"));
 });
