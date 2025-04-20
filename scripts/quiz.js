502243bbc64dbd5f1e61b4baf99714dde807b3d6
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-04-20 12:30:47 +0300

    Cleaning code and debugging from first step

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,28 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     const curCategory = localStorage.getItem("selectedQuizCategory");
-    //console.log("Selected Category:", curCategory);
-    const quizzes = JSON.parse(localStorage.getItem("quizzyQuizzes"));
+    //console.log(curCategory);
+})
 
-    const quizTitle = document.getElementById("quiz-section-title");
-    const quizSection = document.getElementById("quiz-section");
 
-    if (curCategory && quizzes && quizzes[curCategory]) {
-        quizTitle.textContent = curCategory;
-        const questions = quizzes[curCategory];
 
-        questions.forEach((q, index) => {
-            const questionBlock = document.createElement("div");
-            questionBlock.classList.add("question-block");
-
-            const questionEl = document.createElement("h3");
-            questionEl.textContent = `${index + 1}. ${q.question}`;
-            questionBlock.appendChild(questionEl);
-
-            
-        })
-    }
-    
-});
 
 const homeBtn = document.getElementById("home-button");
 if (homeBtn) {
