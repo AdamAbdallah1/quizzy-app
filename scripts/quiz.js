1aa79079720ad7c11c62dec4fe4cb757dbd670ee
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-04-20 12:14:03 +0300

    Getting category and questions

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,14 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
     const curCategory = localStorage.getItem("selectedQuizCategory");
+    //console.log("Selected Category:", curCategory);
 
     if(curCategory) {
         const quizTitle = document.getElementById("quiz-section-title");
-        quizTitle.textContent = `${curCategory}`;
+        if (quizTitle) quizTitle.textContent = curCategory;
     }
 
-})
+    const quizzes = JSON.parse(localStorage.getItem("quizzyQuizzes"));
+    //console.log("All Quizzes:", quizzes);
+
+    if (quizzes && curCategory && quizzes[curCategory]) {
+        const questions = quizzes[curCategory];
+        const firstQuestion = questions[0];
+
+        const questionElement = document.getElementById("quiz-question");
+        if (questionElement) questionElement.textContent = firstQuestion.question;
+    }
+});
 
 const homeBtn = document.getElementById("home-button");
-homeBtn.addEventListener("click", () => {
-    window.location.href ="../pages/home.html";
-})
+if (homeBtn) {
+    homeBtn.addEventListener("click", () => {
+        window.location.href = "../pages/home.html";
+    });
+}
